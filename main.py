cd2668e7ab3a25b1cf40688896443fd80307e097
AbdallahYassin <redaigbaria@rishon1.csf.technion.ac.il>
2020-03-24 19:28:02 +0000

    update

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ import os
 import pickle
 import re
 import shutil
+import time
+import logging
+import json
+
 
 bases_dict = dict()
 replacement_dict = dict()
+
+start_time = 0
 
 def address_breakfun(state):
     # if not state.inspect.address_concretization_add_constraints:
@@ -33,13 +39,15 @@ def address_breakfun(state):
         bases_dict[state.inspect.address_concretization_result[0]] = var_name
         replacement_dict[state.inspect.address_concretization_result[0]] = f"{var_name}(0)"
     else:
-        # depth is 2 (either a new sym-var is being declared or offset calc)
+        # depth is 2 (either a new symbolic-var is being declared or offset calc)
         if expr.op != "__add__":
             print(f"found new op: {expr.op}")
             return
-        childs = list(expr.args)
-        assert len(childs) < 3
-        if len(childs) == 1:
+        children = list(expr.args)
+        # assert len(children) < 3
+        if len(children) > 2:
+            print("warning, an expression with more than 2 children is being relativised")
+        if len(children) == 1:
             if state.solver.eval(expr) in bases_dict:
                 return
             # new var is declared
@@ -49,7 +57,7 @@ def address_breakfun(state):
         else:
             base = None
             offset = None
-            for c in childs:
+            for c in children:
                 if not c.concrete:
                     base = state.solver.eval(c)
                 else:
@@ -59,7 +67,7 @@ def address_breakfun(state):
             replacement_dict[state.inspect.address_concretization_result[0]] = f"{bases_dict[base]}({offset})"
 
 def is_qualified(symbol):
-    avoid = {'main', 'usage'}
+    avoid = {'main', 'usage', 'exit'}
     return symbol.is_function and symbol.is_export and not (symbol.name.startswith("_") or symbol.name in avoid)
 
 
@@ -71,37 +79,50 @@ def get_functions(proj):
     return funcs
 
 
+def get_cfg_functions(proj):
+    funcs = []
+    for f in proj.kb.functions.values():
+
+
+def time_limit_check():
+    global start_time
+    minutes_limit = 1
+    should_stop = time.time() - start_time > (60 * minutes_limit)
+    if should_stop:
+        print("stopped exploration")
+    return should_stop
+
+
 def analyze_func(proj, fun, cfg):
     print(f"started running {fun.name}")
-    call_state = proj.factory.call_state(fun.rebased_addr)
+    call_state = proj.factory.call_state(fun.rebased_addr, add_options={
+        'CALLLESS': True, 'NO_SYMBOLIC_SYSCALL_RESOLUTION': True
+    })
     call_state.inspect.b('address_concretization', when=angr.BP_AFTER, action=address_breakfun)
     sm = proj.factory.simulation_manager(call_state)
     sm.use_technique(angr.exploration_techniques.LoopSeer(cfg=cfg, bound=2))
-    sm.run()
+    global start_time
+    start_time = time.time()
+    sm.run(until=time_limit_check)
     print(f"finished {fun.name}")
     return sm.deadended
 
 
-def main():
-    core_path = "/home/reda/my_binaries"
+def save_functions_names():
+    core_path = "coreutils_bins"
     for proj_name in os.listdir(core_path):
         if os.path.exists(f"dumps/{proj_name}_functions.pkl"):
             continue
         print(f"analysing proj: {proj_name}")
         proj_path = os.path.join(core_path, proj_name)
         try:
-            proj = angr.Project(proj_path)
+            proj = angr.Project(proj_path, auto_load_libs=False)
         except Exception as e:
-            print(f"{proj_name} loading failed")
+            logging.error(f"{proj_name} loading failed")
             print(e)
             continue
-        # proj.analyses.CFGFast()
         funcs = get_functions(proj)
         pickle.dump(funcs, open(f"dumps/{proj_name}_functions.pkl", "wb"))
-        # analysis = dict()
-        # for fun in funcs:
-        #     analysis[fun.name] = analyze_func(proj, fun)
-        # pickle.dump(analysis, open(f"dumps/{proj}_analysis.pkl", "wb"))
 
 
 def block_to_ins(block: angr.block.Block):
@@ -129,42 +150,84 @@ def cons_to_triple(constraint):
     # return f'{constraint.op}|{arg1.replace(" ", "|")}|{arg2.replace(" ", "|")}'
 
 
-def relify(conts):
+def relify(constraints):
     for k, v in replacement_dict.items():
-        conts = re.sub(f"(0x|mem_){format(k, 'x')}[_0-9]*", v, conts)
-    return conts.replace('{UNINITIALIZED}', '')
+        constraints = re.sub(f"(0x|mem_){format(k, 'x')}[_0-9]*", v, constraints)
+    return constraints.replace('{UNINITIALIZED}', '')
 
 
-def train_input():
-    proj = angr.Project("core_utils_bins/ls", auto_load_libs=False)
-    cfg = proj.analyses.CFGFast()
-    funcs = get_functions(proj)
-    output_name = "ls_test.txt"
+def tokenize_function_name(function_name):
+    return "|".join(function_name.split("_"))
+
+
+def generate_dataset(train_binaries, dataset_name):
+    output_name = f"datasets/{dataset_name}.txt"
     output = open(output_name, "w")
-    dataset_name = "assembly"
-    for test_func in funcs:
-        bases_dict.clear()
-        replacement_dict.clear()
-        exec_paths = analyze_func(proj, test_func, cfg)
-        if len(exec_paths) == 0:
-            continue
-        for exec_path in exec_paths:
-            blocks = [proj.factory.block(baddr) for baddr in exec_path.history.bbl_addrs]
-            processsed_code = "|".join(list(filter(None, map(block_to_ins, blocks))))
-            processed_consts = "|".join(list(filter(None, map(cons_to_triple, exec_path.solver.constraints))))
-            relified_consts = relify(processed_consts)
-            output.write(f"{test_func.name} DUM,{processsed_code}|CONS|{relified_consts},DUM\n")
+    analysed_funcs = set()
+    for binary in train_binaries:
+        proj = angr.Project(binary, auto_load_libs=False)
+        cfg = proj.analyses.CFGFast()
+        # funcs = get_functions(proj)
+
+        for test_func in proj.kb.functions.values():
+            if test_func.name in analysed_funcs:
+                print(f"skipping {test_func.name}")
+                continue
+            print(f"analyzing {binary}/{test_func.name}")
+            bases_dict.clear()
+            replacement_dict.clear()
+            try:
+                exec_paths = analyze_func(proj, test_func, cfg)
+                if len(exec_paths) == 0:
+                    continue
+                for exec_path in exec_paths:
+                    blocks = [proj.factory.block(baddr) for baddr in exec_path.history.bbl_addrs]
+                    processsed_code = "|".join(list(filter(None, map(block_to_ins, blocks))))
+                    processed_consts = "|".join(list(filter(None, map(cons_to_triple, exec_path.solver.constraints))))
+                    relified_consts = relify(processed_consts)
+                    output.write(f"{tokenize_function_name(test_func.name)} DUM,{processsed_code}|CONS|{relified_consts},DUM\n")
+            except Exception as e:
+                logging.error(str(e))
+                logging.error(f"got an error while analyzing {test_func.name}")
+            analysed_funcs.add(test_func.name)
     output.close()
-    shutil.copy2(output_name, f"code2seq/{dataset_name}.train.raw.txt")
-    shutil.copy2(output_name, f"code2seq/{dataset_name}.val.raw.txt")
-    shutil.copy2(output_name, f"code2seq/{dataset_name}.test.raw.txt")
-    os.remove(output_name)
+    # shutil.copy2(output_name, f"code2seq/{dataset_name}.train.raw.txt")
+    # shutil.copy2(output_name, f"code2seq/{dataset_name}.val.raw.txt")
+    # shutil.copy2(output_name, f"code2seq/{dataset_name}.test.raw.txt")
+    # os.remove(output_name)
+
+
 
 
 if __name__ == "__main__":
-    # main()
-    train_input()
+    # note: some functions are shared among most of the binaries,
+    # should consider removing them from the learning scheme, or adding them just once in the dataset
+    binaries = os.listdir("coreutils_bins")
+    binaries.sort()
+    binaries = [f"coreutils_bins/{binary}" for binary in binaries]
+    # mv
+    generate_dataset(binaries[51:52], "new_test")
     exit()
+    # A test to detremine wether to use CFGFast or EmulatedCFG for finding functions in the binary... it turns out
+    # should use CFGFast, but remove all undefined symbols that it adds (starts with sub_xxx)
+    # binaries = ['core_utils_bins/ls']
+    # for bin in binaries:
+    #     proj = angr.Project(bin, auto_load_libs=False)
+    #     # efg = proj.analyses.CFGEmulated()
+    #     cfg = proj.analyses.CFGFast()
+    #     cfg_functions = {f.name for f in cfg.kb.functions.values()}
+    #     # efg_functions = {f.name for f in efg.kb.functions.values()}
+    #
+    #     # with open(f"jsons/{os.path.basename(bin)}.json", "w") as f:
+    #     #     json.dump({"cfg": list(cfg_functions), "efg": list(efg_functions),
+    #     #                "diff": list(cfg_functions.symmetric_difference(efg_functions))},
+    #     #               f, indent=4)
+    #     # pickle.dump(efg_functions, open("emulated_ls_test.pkl", "wb"))
+    #     pickle.dump(cfg_functions, open("cfg_ls_test.pkl", "wb"))
+    #     print(f"finished {bin}")
+    # exit()
+    # generate_dataset(binaries[0:20], dataset_name="overfitting_test")
+    # exit()
     hist = dict()
     p = "dumps"
     for name in os.listdir(p):
@@ -180,5 +243,5 @@ if __name__ == "__main__":
 
     print(c)
     # move binaries
-    #  ls -al  | grep ^-rwxr | awk '{print $(NF)}' | while read line;do mv $line ~/my_binaries/;done
+    #  ls -al  | grep ^-rwxr | awk '{print $(NF)}' | while read line;do cp $line ~/sec_proj/coreutils_bins;done
     # 
